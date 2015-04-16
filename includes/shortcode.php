afeb135e15524823edec56d25547205c4875b89e
PartyPlugins <PartyPlugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-16 22:17:58 +0000

    removed h1
    git-svn-id: https://plugins.svn.wordpress.org/sns-subscription-form-for-amazon-web-services-push-notifications/tags/1.0@1136675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -37,7 +37,6 @@ if ($msg == ""):
 
 	?>
 										<form action="" method="post" class="basic-grey">
-										<h1 class="basic-grey">Subscription Form</h1>
 										<input type="hidden" name="tarn" value="<?php echo $attr['topic_id'];?>" />
 										<label for="protocol">
 										<span>Subscribe ON :</span></label>
